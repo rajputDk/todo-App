58ed6559e86d6428330ba6b5a94635ef7b3f9726
deepak singh <dhonideepaksingh123@gmail.com>
2023-01-25 11:34:25 +0530

    design complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css"
+
 
 function App() {
-  return (
-    <div className="App">
-     <h1>todo</h1>
-    </div>
-  );
-}
+ 
 
-export default App;
+  
+  return (
+  <div>
+      <div className="container"> 
+      <div className="h">      
+      <h1>To-Do List App</h1>  
+      </div>
+          <input type="text" id="in" placeholder="Add Todo"></input>
+          <button className="btn1">Add</button>
+      </div>
+    </div>
+   
+  )
+  }
+
+export default App
